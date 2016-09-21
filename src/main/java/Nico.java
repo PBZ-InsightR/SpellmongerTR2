f84508855ec94cfc4c10d2d0308bb09dabcb9889
nicolasthirouin <nico.thirouin@gmail.com>
2016-09-21 12:18:44 +0200

    test

--- src/main/java/Nico.java
+++ src/main/java/Nico.java
@@ -1,9 +1,18 @@
 import com.sun.deploy.uitoolkit.ui.ConsoleWindow;
 
 /**
- * Created by Nico on 21/09/2016.
+ * Created by Nico on 21/09/2016. changement
  */
 public class Nico {
+    /**
+     * @param a premier chiffre
+     * @param b deuxieme chiffre
+     * @return resultats
+     */
+    public int multiplication(int a, int b) {
+        return (a + b) / 2;
+
+    }
 }
 
 
