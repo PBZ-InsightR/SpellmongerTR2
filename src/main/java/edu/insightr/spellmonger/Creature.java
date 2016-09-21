9e39f018da9c30cbf8cf7dfe25cc9eb9c6a9d39c
Lilly3010 <aurelie.le_calvez@devinci.fr>
2016-09-21 11:30:55 +0200

    Add "Creature" class

---
+++ src/main/java/edu/insightr/spellmonger/Creature.java
@@ -0,0 +1,28 @@
+package edu.insightr.spellmonger;
+
+/**
+ * Created by Aurélie on 21/09/2016.
+ * Used to define creatures and damages they do
+ */
+public class Creature {
+    private String name;
+    private int damage;
+
+    public Creature(String name) {
+        this.name = name;
+        switch (name) {
+            case "eagle":
+                this.damage = 1;
+                break;
+            case "wolf":
+                this.damage = 2;
+                break;
+            case "bear":
+                this.damage = 3;
+                break;
+            default:
+                this.damage = 0;
+                break;
+        }
+    }
+}
