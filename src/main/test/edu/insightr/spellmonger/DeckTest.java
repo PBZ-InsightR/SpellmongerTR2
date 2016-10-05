a442b82335e905c28e3d537647cb5fd21764f0b0
Branluchon <adrianpothuaud@gmail.com>
2016-10-05 09:14:33 +0200

    news in unit tests

--- src/main/test/edu/insightr/spellmonger/DeckTest.java
+++ src/main/test/edu/insightr/spellmonger/DeckTest.java
@@ -6,32 +6,43 @@ import static org.junit.Assert.*;
 
 /**
  * Created by Jessica on 03/10/2016.
+ * Modified by Adrian
  * Test methods of class Desk
  */
 public class DeckTest {
 
-    private Deck deck = new Deck();
-    private Card cardC = new Creature("Eagle", 1);
-    private Card cardR = new Ritual("Curse", -3);
+    /*
+    Les tests unitaires doivent être indépendants les uns des autres:
+    La méthode Before va regénérer un deck et des cartes avant chaque test
+     */
+    @Before
+    public void initializeTests() {
+        private Deck deck = new Deck();/*empty deck*/
+        private Card cardC = new Creature("Eagle", 1);
+        private Card cardR = new Ritual("Curse", -3);
+    }
 
     @Test
     public void testAddCard() throws Exception {
         assertTrue(deck.addCard(cardC));
-        assertFalse(deck.addCard(cardR));
+        /*
+        Ajouter une assertion (nécessite une nouvelle méthode?): deck "contains" cardC
+         */
+        assertFalse(deck.addCard(cardR));/*Pourquoi faux?*/
     }
 
     @Test
     public void testRemoveCard() throws Exception {
-        /* dans la class Deck:
-        public boolean removeCard(Card c) {return cards.remove(c);}
-        méthode booléenne, pourtant on ne retroune pas de true/false (?)
-         */
-        assertFalse(deck.removeCard(cardR)); // doit retourner true et non false
+        assertFalse(deck.removeCard(cardR)); // doit retourner false car le deck est réinitialisé avant chaque test
+        //ajout
+        deck.addCard(cardR);
+        assertTrue(deck.removeCard(cardR)); //maintenant doit etre true
     }
 
     @Test
     public void testGetDamages() throws Exception {
-        deck.addCard(cardC);
+        //deck réinitalisé sans cartes
+        deck.addCard(cardC);//ajout carte avec 1 dommage
         assertEquals(1, deck.getDamages());
     }
 
