9739031e2494fa236638116694ee7da199ce795d
Karim Kidiss <ozerioss@gmail.com>
2016-10-14 11:33:30 +0200

    Create Controller.java
    Should also be in another package

---
+++ src/main/java/Controller.java
@@ -0,0 +1,13 @@
+package testfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Ozerioss on 14/10/2016.
+ */
+public class Controller {
+}
