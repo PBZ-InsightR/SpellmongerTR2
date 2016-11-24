0805cf4687c9f7431078fc762e5ec53e830cd85e
Karim Kidiss <ozerioss@gmail.com>
2016-11-24 09:47:34 +0100

    Update Controller.java

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,13 +1,46 @@
 package testfx;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
+import edu.insightr.spellmonger.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created by Ozerioss on 14/10/2016.
  */
-public class Controller {
+public class Controller implements Initializable{
+    @FXML
+    private ChoiceBox<String> gamechoice_box;
+
+    /*@FXML
+    private Button launch_button;
+
+    @FXML
+    private Button exit_button;*/
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        gamechoice_box.getItems().addAll("       IA vs IA       ", "      Player vs IA       ", "      Player vs Player      ");
+
+
+    }
+
+    public Controller()
+    {
+
+
+    }
+
 }
